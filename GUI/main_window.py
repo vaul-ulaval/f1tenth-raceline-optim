d98c9b169ea8fd312651beeaff7dac4c9d22ee27
David Ferland <david.ferland.1@ulaval.ca>
2023-11-28 16:43:27 -0500

    save functionality and small clean up

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,42 +1,46 @@
 import sys
-import numpy as np
 import yaml
 import os
+import csv
 
 from gen_centerline import gen_centerline, display_centerline
 from PyQt5 import uic
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox, QSlider, QFileDialog, QGraphicsView, QGraphicsScene, \
-    QGraphicsPixmapItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox, QSlider, QFileDialog, QGraphicsView, \
+    QGraphicsScene, \
+    QGraphicsPixmapItem, QLabel
 
 
 class UI(QMainWindow):
-    centerline_file = ""
-
     def __init__(self):
         super(UI, self).__init__()
         uic.loadUi('MainWindow.ui', self)
 
-        self.loadMapButton = self.findChild(QPushButton, 'loadMapButton')
-        self.loadMapButton.clicked.connect(self.load_file)
-
         self.centerLineView = self.findChild(QGraphicsView, 'centerLineView')
-        self.scene = QGraphicsScene()
-        self.centerLineView.setScene(self.scene)
-        
+
         self.thresholdSlider = self.findChild(QSlider, 'thresholdSlider')
+        self.thresholdValue = self.findChild(QLabel, 'thresholdValue')
+        self.thresholdSlider.valueChanged.connect(
+            lambda: self.thresholdValue.setText(str(self.thresholdSlider.value()/100))
+        )
+
         self.reverseCheckBox = self.findChild(QCheckBox, 'reverseCheckBox')
 
         self.genCenterlineButton = self.findChild(QPushButton, 'genCenterlineButton')
+        self.genCenterlineButton.setEnabled(False)
         self.genCenterlineButton.clicked.connect(self.generate_centerline)
 
+        self.loadMapButton = self.findChild(QPushButton, 'loadMapButton')
+        self.loadMapButton.clicked.connect(self.load_map)
+
         self.saveCenterlineButton = self.findChild(QPushButton, 'SaveCenterLineButton')
+        self.saveCenterlineButton.setEnabled(False)
+        self.saveCenterlineButton.clicked.connect(self.save_centerline)
 
         self.show()
 
-
-    def load_file(self):
-        map_yaml_path, _ = QFileDialog.getOpenFileName(self, 'Open file', '..', "Image files (*.yaml)")
+    def load_map(self):
+        map_yaml_path, _ = QFileDialog.getOpenFileName(self, 'Open file', '../racetracks/maps', "Image files (*.yaml)")
         base_path = os.path.split(map_yaml_path)[0]
         with open(map_yaml_path, 'r') as yaml_stream:
             map_metadata = yaml.safe_load(yaml_stream)
@@ -45,32 +49,45 @@ class UI(QMainWindow):
             self.map_img_path = os.path.join(base_path, map_metadata['image'])
 
         img = QImage(self.map_img_path)
-        pic = QGraphicsPixmapItem()
-        pic.setPixmap(QPixmap.fromImage(img))
-        self.scene.setSceneRect(0, 0, self.centerLineView.width(), self.centerLineView.height())
-        self.scene.addItem(pic)
-
-        # map_img = cv2.imread(map_img_path, cv2.IMREAD_GRAYSCALE)
-        # self.map_img = np.array(map_img)
+        self.draw_image(img)
+        self.genCenterlineButton.setEnabled(True)
 
     def generate_centerline(self):
-        waypoints, track_widths, transformed_data = gen_centerline(
-            self.map_img_path, 
+        waypoints, track_widths, self.transformed_data = gen_centerline(
+            self.map_img_path,
             self.map_resolution,
             self.origin,
-            self.thresholdSlider.value()/100,
+            float(self.thresholdValue.text()),
             self.reverseCheckBox.isChecked()
-            )
+        )
         img = display_centerline(self.map_img_path, waypoints)
-
         img = QImage(img, img.shape[1], img.shape[0], QImage.Format_RGB888)
-        pic = QGraphicsPixmapItem()
-        pic.setPixmap(QPixmap.fromImage(img))
-        self.scene.setSceneRect(0, 0, self.centerLineView.width(), self.centerLineView.height())
-        self.scene.addItem(pic)
+
+        self.draw_image(img)
+        self.saveCenterlineButton.setEnabled(True)
 
     def save_centerline(self):
+        file_name, _ = QFileDialog.getSaveFileName(self, 'Save file', '../racetracks/centerlines', "CSV files (*.csv)")
+        header = ['x_m', 'y_m', 'w_tr_right_m', 'w_tr_left_m']
+
+        with open(f"{str(file_name)}.csv", 'w', newline="") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(header)
+            for row in self.transformed_data:
+                round_row = [round(elem, 3) for elem in row]
+                csv_writer.writerow(round_row)
+
+    def draw_image(self, img):
+        self.scene = QGraphicsScene()
+        self.centerLineView.setScene(self.scene)
+        pic = QGraphicsPixmapItem()
+        pic.setPixmap(QPixmap.fromImage(img))
+        self.scene.addItem(pic)
+
+
+    def find_center_point(self):
         pass
+
 
 app = QApplication(sys.argv)
 
